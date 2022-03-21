447f72af328776dd108c89801b2a403221e2e2f4
Alquimara <alquimara.a.braz@gmail.com>
2022-03-21 18:28:14 -0300

    Desenvolvimento da Landing Page Marun - teste

---
+++ index.js
@@ -0,0 +1,9 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+
+var imgcardsocail = document.getElementById("imgsocial");
+var imgcardsocailhover = document.getElementsByClassName("img-card-social-hover");
+
+
+
